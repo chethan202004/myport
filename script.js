f7fc83ac609bef8799c2caee4fe94175b55508d1
user-name <email-address>
2024-11-13 12:58:31 +0530

    first commit

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
-function toggleMenu() {
-  const menu = document.querySelector(".menu-links");
-  const icon = document.querySelector(".hamburger-icon");
-  menu.classList.toggle("open");
-  icon.classList.toggle("open");
-}
+document.querySelectorAll('.gallery-container').forEach(gallery => {
+  let currentIndex = 0;
+  const images = gallery.querySelectorAll('.gallery-image');
+  const totalImages = images.length;
+
+  function showNextImage() {
+    images[currentIndex].classList.remove('active');
+    currentIndex = (currentIndex + 1) % totalImages; // Loops back to the first image
+    images[currentIndex].classList.add('active');
+  }
+
+  // Start the image rotation for each gallery individually every 3 seconds
+  setInterval(showNextImage, 1000);
+});
